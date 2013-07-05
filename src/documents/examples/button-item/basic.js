488afe70da675b263539614f56b92c76d8efd1f9
RONALD_FANCHER <RONALD.FANCHER@LIFERAY.COM>
2013-07-05 15:00:36 -0700

    Improve code format
    Improve code format

--- src/documents/examples/button-item/basic.js
+++ src/documents/examples/button-item/basic.js
@@ -1,15 +1,15 @@
 AUI().use(
-  'aui-button-item',
-  function(A) {
-    new A.ButtonItem(
-      {
-        handler: function() {
-          alert('Here is some info, triggered by an aui button!');
-        },
-        icon: 'info',
-        render: '#buttons',
-        title: 'click this for an alert'
-      }
-    );
-  }
+	'aui-button-item',
+	function(A) {
+		new A.ButtonItem(
+			{
+				handler: function() {
+					alert('Here is some info, triggered by an aui button!');
+				},
+				icon: 'info',
+				render: '#buttons',
+				title: 'click this for an alert'
+			}
+		);
+	}
 );
